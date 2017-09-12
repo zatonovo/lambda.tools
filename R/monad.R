1bc280e5b1b90efdddcb93c133774fddf5ff53f8
Brian Lee Yung Rowe <rowe@zatonovo.com>
2017-09-12 10:44:31 -0400

    Add monads

---
+++ R/monad.R
@@ -0,0 +1,24 @@
+Maybe(a) %:=% a
+Just(a) %:=% Maybe(a)
+Nothing() %:=% Maybe(NA)
+      
+mreturn(x) %as% Just(x)
+
+# Bind operator
+m %>>=% g %::% Nothing : Function : Maybe
+m %>>=% g %as% m
+    
+m %>>=% g %::% Just : Function : Maybe
+m %>>=% g %as% g(m)
+
+
+# Regular composition f(g(x))
+f %.% g %:=% function(...) f(g(...))
+
+# Monad composition g(f(x))
+f %>=>% g %:=% { function(x) f(x) %>>=% g }
+
+# Monad composition f(g(x))
+f %<=<% g %:=% { function(x) g(x) %>>=% f }
+
+
